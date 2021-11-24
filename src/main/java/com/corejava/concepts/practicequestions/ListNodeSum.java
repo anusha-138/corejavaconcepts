fa5a40554edd538694ce2865584400d0eb5ebda8
anusha-138 <anusha.b138@gmail.com>
2021-11-24 18:42:29 +0530

    Practice Questions

---
+++ src/main/java/com/corejava/concepts/practicequestions/ListNodeSum.java
@@ -0,0 +1,48 @@
+package com.corejava.concepts.practicequestions;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ListNodeSum {
+    /**You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+     You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+     Example 1:
+
+
+     Input: l1 = [2,4,3], l2 = [5,6,4]
+     Output: [7,0,8]
+     Explanation: 342 + 465 = 807.
+     Example 2:
+
+     Input: l1 = [0], l2 = [0]
+     Output: [0]
+     Example 3:
+
+     Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+     Output: [8,9,9,9,0,0,0,1]
+     Constraints:
+
+     The number of nodes in each linked list is in the range [1, 100].
+     0 <= Node.val <= 9
+     It is guaranteed that the list represents a number that does not have leading zeros.
+     **/
+    public static List<Integer> sumList(List<Integer> l1,List<Integer> l2) {
+    l2.sort(Comparator.reverseOrder());
+    String s="";
+        for (Integer l : l2) {
+            s= new StringBuilder().append(s).append(l).toString();
+        }
+        int n2=Integer.parseInt(s);
+        l1.sort(Comparator.reverseOrder());
+        String s1="";
+        for (Integer l : l1) {
+            s= new StringBuilder().append(s).append(l).toString();
+        }
+        int n1=Integer.parseInt(s);
+        String sum= String.valueOf(n1+n2);
+        return null;
+    }
+
+}
